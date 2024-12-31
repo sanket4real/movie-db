f6b669b84e66a294039e3f58a6d9f4cad1c156ce
sanket4real <desaisanket68@gmail.com>
2024-12-31 12:20:52 +0530

    added favourites component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,36 +1,83 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text, Button } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { Toaster, toaster } from "@/components/ui/toaster";
 import { Movie } from "../types/movie";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { IoMdHeart } from "react-icons/io";
 
 interface Props {
 	movie: Movie;
 }
 
 export const MovieCard = ({ movie }: Props) => {
+	const [isFavorite, setIsFavorite] = useState(false);
+
+	useEffect(() => {
+		const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+		setIsFavorite(favorites.some((fav: Movie) => fav.id === movie.id));
+	}, [movie.id]);
+
+	const toggleFavorite = () => {
+		const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+
+		if (isFavorite) {
+			const newFavorites = favorites.filter((fav: Movie) => fav.id !== movie.id);
+			localStorage.setItem("favorites", JSON.stringify(newFavorites));
+			setIsFavorite(false);
+			toaster.create({
+				title: "Removed from favorites",
+				duration: 2000,
+			});
+		} else {
+			favorites.push(movie);
+			localStorage.setItem("favorites", JSON.stringify(favorites));
+			setIsFavorite(true);
+			toaster.create({
+				title: "Added to favorites",
+				duration: 2000,
+			});
+		}
+	};
+
 	return (
-		<Box
-			borderRadius="lg"
-			overflow="hidden"
-			boxShadow="md"
-			transition="transform 0.2s"
-			_hover={{ transform: "scale(1.02)" }}>
-			<Image
-				src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-				alt={movie.title}
-				width="100%"
-				height="auto"
-				onError={(e) => {
-					(e.target as HTMLImageElement).src =
-						"https://fakeimg.pl/400x600?text=img&font=bebas";
-				}}
-			/>
-			<Box p={4}>
-				<Text fontSize="xl" fontWeight="bold">
-					{movie.title}
-				</Text>
-				<Text color="gray.600">
-					{movie.release_date ? new Date(movie.release_date).getFullYear() : "N/A"}
-				</Text>
+		<>
+			<Toaster />
+			<Box
+				position={"relative"}
+				borderRadius="lg"
+				overflow="hidden"
+				boxShadow="md"
+				transition="transform 0.2s"
+				_hover={{ transform: "scale(1.02)" }}>
+				<Button
+					position={"absolute"}
+					top={2}
+					right={2}
+					onClick={toggleFavorite}
+					colorScheme={isFavorite ? "red" : "blue"}
+					size="sm"
+					mt={2}>
+					{isFavorite ? <IoMdHeart fill="red" /> : <IoIosHeartEmpty />}
+				</Button>
+				<Image
+					src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+					alt={movie.title}
+					width="100%"
+					height="auto"
+					onError={(e) => {
+						(e.target as HTMLImageElement).src =
+							"https://fakeimg.pl/400x600?text=img&font=bebas";
+					}}
+				/>
+				<Box p={4}>
+					<Text fontSize="xl" fontWeight="bold">
+						{movie.title}
+					</Text>
+					<Text color="gray.600">
+						{movie.release_date ? new Date(movie.release_date).getFullYear() : "N/A"}
+					</Text>
+				</Box>
 			</Box>
-		</Box>
+		</>
 	);
 };
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,5 +1,48 @@
+import { MovieCard } from "@/components/MovieCard";
+import { Container, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+
+type Movie = {
+	id: string;
+	title: string;
+};
+
 const Favorites = () => {
-	return <div>Favorites</div>;
+	const [favorites, setFavorites] = useState([]);
+
+	useEffect(() => {
+		const storedFavorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+		setFavorites(storedFavorites);
+	}, []);
+
+	return (
+		<Container>
+			<Heading size="2xl" as="h1" mt={4} mb={6} textAlign="center">
+				My Favorite Movies
+			</Heading>
+
+			{favorites.length === 0 ? (
+				<Text textAlign="center" mt={4}>
+					No favorite movies added yet.
+				</Text>
+			) : (
+				<Grid
+					templateColumns={[
+						"repeat(1, 1fr)",
+						"repeat(2, 1fr)",
+						"repeat(3, 1fr)",
+						"repeat(4, 1fr)",
+					]}
+					gap={["4", "5", "6"]}>
+					{favorites.map((movie) => (
+						<GridItem key={(movie as Movie).id}>
+							<MovieCard movie={movie} />
+						</GridItem>
+					))}
+				</Grid>
+			)}
+		</Container>
+	);
 };
 
 export default Favorites;
